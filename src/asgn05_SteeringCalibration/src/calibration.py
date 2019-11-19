d649302ca1ff52dae24c1cfc855c3f3a3a58cf68
rothursuul <per.guelzow@gmail.com>
2019-11-19 11:52:12 +0100

    final assg05

--- src/asgn05_SteeringCalibration/src/calibration.py
+++ src/asgn05_SteeringCalibration/src/calibration.py
@@ -3,62 +3,89 @@ import rospy
 from autominy_msgs.msg import SteeringFeedback, SpeedCommand, NormalizedSteeringCommand
 from nav_msgs.msg import Odometry
 
-#set the current car
-currentCar = 7
-
+rospy.init_node("calibration", anonymous=False)
 #---Subscribe to the Steering_Angle Topic to get the current value for calculation---
 steeringAngle = 0
-def get_info():
 def callback_steering(data):
     global steeringAngle
     steeringAngle = data.value
 
 def get_current_steering_angle():
-    rospy.init_node("steeringAngle", anonymous=False)
     rospy.Subscriber("/sensors/arduino/steering_angle", SteeringFeedback, callback_steering)
-    rospy.spin()
-print("here1")
+    rospy.sleep(1)
 
 get_current_steering_angle()
-print("Test: The Current_Steering_Angle is: %s", steeringAngle)
+print("Test: The Current_Steering_Angle is: ", steeringAngle)
 #------------------------------------------------------------------------------------
-print("here2")
 
 #---Subscribe to the current position---
-currentPosition = 0
+currentPosition_x = 0
+currentPosition_y = 0
+currentPosition_z = 0
+
 def callback_position(data):
-    global currentPosition
-    currentPosition = data.value
+    global currentPosition_x
+    global currentPosition_y
+    global currentPosition_z
+    currentPosition_x = data.pose.pose.position.x
+    currentPosition_y = data.pose.pose.position.y
+    currentPosition_z = data.pose.pose.position.z
+
 
 def get_current_position():
-    rospy.init_node("currentPosition", anonymous=False)
-    rospy.Subscriber("/communication/gps/%s",currentCar, Odometry, callback_position)
-    rospy.spin()
+    rospy.Subscriber("/communication/gps/11", Odometry, callback_position)
+    rospy.sleep(1)
 get_current_position()
-print("Test: The Current_Steering_Position is: %s", currentPosition)
+print("x:", currentPosition_x, "y:", currentPosition_y, "z:", currentPosition_z)
 #----------------------------------------
 
 #---Starting the calibration---
 #Values and Publishers
 speed = SpeedCommand()
-speed.value = 0
+speed.value = 0.2
 steering = NormalizedSteeringCommand()
-steering.value = 0
+steering.value = 1
 
-rospy.init_node("driveNode")
 steeringPublisher = rospy.Publisher("/actuators/steering_normalized", NormalizedSteeringCommand, queue_size=10)
 speedPublisher = rospy.Publisher("/actuators/speed", SpeedCommand, queue_size=10)
+rospy.sleep(1)
 
-#1)Driving in a circle leftward direction
+#1)Steering angle calibration
+#get_current_position()
+steeringPublisher.publish(steering)
+speedPublisher.publish(speed)
+rospy.sleep(3)
+print("x:", currentPosition_x, "y:", currentPosition_y, "z:", currentPosition_z)
+rospy.sleep(3)
+print("x:", currentPosition_x, "y:", currentPosition_y, "z:", currentPosition_z)
+speed.value = 0
+speedPublisher.publish(speed)
 
-#noting the current position
-get_current_position()
-starting_pos_1 = currentPosition
-#driving
-steeringPublisher.publish(0.1)
-speedPublisher.publish(0.1)
-print("here!")
-#keep driving until back at the start
-#while starting_pos_1 != currenPosition:
- #   speedPublisher.publish(0.1)
-  #  get_current_position()
+#2)calibration
+#2-1)leftward direction
+steering.value = 1.0
+steeringPublisher.publish(steering)
+while currentPosition_y != 2.2:
+    speed.value = 0.1
+    speedPublisher.publish(speed)
+speed.value = 0.0
+speedPublisher.publish(speed)
+print("x:", currentPosition_x, "y:", currentPosition_y, "z:", currentPosition_z)
+
+#2-2) straight
+target_pos_1 = currentPosition_x + 2
+while currentPosition_x != target_pos_1:
+    speed.value = 0.1
+    speedPublisher.publish(speed)
+speed.value = 0.0
+speedPublisher.publish(speed)
+
+#2-3)leftward direction
+steering.value = -1.0
+steeringPublisher.publish(steering)
+while currentPosition_y != 2.2:
+    speed.value = 0.1
+    speedPublisher.publish(speed)
+speed.value = 0.0
+speedPublisher.publish(speed)
+print("x:", currentPosition_x, "y:", currentPosition_y, "z:", currentPosition_z)
