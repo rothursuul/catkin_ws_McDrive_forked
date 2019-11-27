47bba67ed809c2b08f7ffba99c4d798f349923b8
rothursuul <per.guelzow@gmail.com>
2019-11-27 21:15:49 +0100

    fixed image sourcing from bagfile

--- src/asgn07/src/line_segmentation.py
+++ src/asgn07/src/line_segmentation.py
@@ -4,12 +4,18 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
 
 #Getting the infrared Image as a matrix
-image = []
+image = Image()
+encoding = ""
+bridge = CvBridge()
 def callback(data):
     global image
-    image = data.encoding
+    global encoding
+    encoding = data.encoding
+    image = data
 
 #Getting the data from the bagfile
 rospy.init_node("line_segmentation", anonymous=False)
@@ -19,8 +25,13 @@ rospy.sleep(1)
 
 #Working on the Image
 print(type(image))
-print(image)
-#ret, thresh_bin = cv.threshold(image,254,255,cv.THRESH_BINARY)
+print(encoding)
+#print(image)
+cv_image = bridge.imgmsg_to_cv2(image, encoding)
+cv.imshow("Image window", cv_image)
+cv.waitKey(0)
 
+#Segmenting the image for white lines
+#ret, thresh_bin = cv.threshold(image,254,255,cv.THRESH_BINARY)
 #plt.imshow(thresh_bin, 'gray')
 #plt.show()
