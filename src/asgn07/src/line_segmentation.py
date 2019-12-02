6263a64dcbc42d067bf0920a5488fccd1bdcbf35
eRichter09 <emilius.richter@gmail.com>
2019-12-02 14:26:53 +0100

    Added RANSAC to asgn07

--- src/asgn07/src/line_segmentation.py
+++ src/asgn07/src/line_segmentation.py
@@ -36,8 +36,43 @@ ret, thresh_bin = cv.threshold(cv_image,254,255,cv.THRESH_BINARY)
 #Setting upper and lower part to zero and showing the resulting image
 thresh_bin[:150,:] = 0
 thresh_bin[320:,:] = 0
-#plt.imshow(thresh_bin, 'gray')
-#plt.show()
+plt.imshow(thresh_bin, 'gray')
+plt.show()
 
 imagePublisher.publish(bridge.cv2_to_imgmsg(cv_image, encoding))
 rospy.spin()
+
+
+def linear_fit(p1,p2):
+	m = (p2[1]-p1[1])/(p2[0]-p1[0])
+	b = p1[1]-m*p1[0]
+	return m,b
+
+N = 1e10
+sample_count = 0
+while N > sample_count:
+	#choosing random sample and computing linear fit parameters
+	num_points = len(white_pixels)
+	idx1 = np.random.randint(0,num_points)
+	idx2 = np.random.randint(0,num_points)
+	p1 = white_pixels[idx1]
+	p2 = white_pixels[idx2]
+	m,b = linear_fit(p1,p2)
+
+	#number of inliers
+	t = np.sqrt(3.84)*std_dev
+	y_model = m*white_pixels[:,0]+b
+	num_inliers = 0
+	for i in range(num_points):
+		if y_model[i] < white_pixels[1,i] + t:
+			num_inliers +=1
+
+	#recomputing N
+	s = 2
+	p = 0.95
+	e = 1-(num_inliers/num_points)
+	N = np.log(1-p)/np.log(1-(1-e)**s)
+	sample_count +=1
+
+
+
