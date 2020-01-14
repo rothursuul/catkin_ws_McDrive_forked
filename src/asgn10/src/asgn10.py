906fd43eaf32ddeb629521aeb87822b4bbe9eb74
rothursuul <per.guelzow@gmail.com>
2020-01-14 11:10:12 +0100

    asgn10

--- src/asgn10/src/asgn10.py
+++ src/asgn10/src/asgn10.py
@@ -1,6 +1,8 @@
 import rospy
 import numpy as np
 #import scipy as scipy
+from visualization_msgs.msg import Marker
+
 
 lane1 = np.load("./lane1.npy")
 lane2 = np.load("./lane2.npy")
@@ -8,4 +10,10 @@ lane2 = np.load("./lane2.npy")
 print(len(lane1))
 print(len(lane2))
 
-#was heisst hier "arc length", ist da die distanz zum vorherigen Punkt gemeint?
+
+rospy.init_node("test", anonymous=False)
+spline = rospy.Publisher("spline", Marker, queue_size=100)
+rospy.sleep(0)
+marker = Marker()
+spline.publish(marker)
+rospy.spin()
